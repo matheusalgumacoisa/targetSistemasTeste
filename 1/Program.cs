7a35d144ca077de4e05c045fbbb96d2c21753979
matheusalgumacoisa <matheusantoniorss@outlook.com>
2025-01-12 16:19:51 -0300

    Questão 1 e 5

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int indice = 13;
+int soma = 0;
+int k = 0;
+
+while( k < indice){
+    k = k + 1;
+    soma = soma + k;
+}
+
+Console.WriteLine("Soma: "+ soma);
---
+++ 5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Entre com uma string");
+string s =  Console.ReadLine();
+string aux = "";
+
+for (int i = s.Length-1; i >= 0; i--){
+    aux += s[i];
+}
+
+Console.WriteLine("String invertida: "+aux);
